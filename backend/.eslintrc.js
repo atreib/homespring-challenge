09c23f7ffabe4e84f296487a6874dfb221e99931
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-26 19:39:34 -0300

    chore: setup eslint configuration file

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
+  env: {
+    esnext: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+  settings: {
+    "import/resolver": {
+      typescript: {},
     },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  },
+};
