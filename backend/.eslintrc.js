bdcfd40ac12ca4733d6d6d01eaf1f810b48ae48d
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-26 19:47:21 -0300

    chore: setup prettier on eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
   env: {
-    esnext: true,
+    es2021: true,
     node: true,
   },
-  extends: ["airbnb-base", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+  },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {},
     },
   },
