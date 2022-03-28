8918958c516a07e0d8b6824260ef0a334317fa0f
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 20:07:59 -0300

    feature: shoud paginate on get books service

--- backend/src/domain/use-cases/books/get.ts
+++ backend/src/domain/use-cases/books/get.ts
@@ -1,5 +1,11 @@
 import { Book } from '../../models/book';
+import { Paginated } from '../pagination';
+
+export interface IGetBooksServiceDTO {
+  search?: string;
+  page?: number;
+}
 
 export interface IGetBooksService {
-  handle(search?: string): Promise<Book[]>;
+  handle(options: IGetBooksServiceDTO): Promise<Paginated<Book>>;
 }
