52b50e0220aa7206460eb02b8791ba7fd09a712a
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 11:02:57 -0300

    test: expect books repository to be called

--- backend/src/domain/services/books/get.spec.ts
+++ backend/src/domain/services/books/get.spec.ts
@@ -1,4 +1,23 @@
+import { GetBooksService } from './get';
+import { IBooksRepository } from '../../contracts/books-repository';
+
+interface ISutTypes {
+  sut: GetBooksService;
+  booksRepositoryStub: IBooksRepository;
+}
+
+const makeSut = (): ISutTypes => {
+  const sut = new GetBooksService();
+  return { sut, booksRepositoryStub: {} as IBooksRepository };
+};
+
 describe('Get Books Services Test Suite', () => {
-  it.todo('Should call Books Repository to get books');
+  it('Should call Books Repository to get books', async () => {
+    const { sut, booksRepositoryStub } = makeSut();
+    const spy = jest.spyOn(booksRepositoryStub, 'get');
+    await sut.handle();
+    expect(spy).toHaveBeenCalled();
+  });
+
   it.todo('Should return books from the Books Repository');
 });
