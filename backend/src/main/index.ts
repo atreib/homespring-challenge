ff72cb2f88c542fb93775a55bf4b406eca929779
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 16:49:24 -0300

    feature: create api entrypoint

---
+++ backend/src/main/index.ts
@@ -0,0 +1,7 @@
+import { app } from './app';
+
+const port = 3000;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Homespring Challenge listening on port ${port}`);
+});
