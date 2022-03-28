ebc27b75d48a89df5ce3a829f7e8bd9819bfbbd4
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 10:57:53 -0300

    feature: create get books service

---
+++ backend/src/domain/services/books/get.ts
@@ -0,0 +1,8 @@
+import { Book } from '../../models/book';
+import { IGetBooksService } from '../../use-cases/books/get';
+
+export class GetBooksService implements IGetBooksService {
+  handle(): Promise<Book[]> {
+    throw new Error();
+  }
+}
