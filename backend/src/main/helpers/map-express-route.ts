11fb7bd56c47f6316bab7485e7a65b51aec37061
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 19:18:37 -0300

    feature: provide querystring to http services

--- backend/src/main/helpers/map-express-route.ts
+++ backend/src/main/helpers/map-express-route.ts
@@ -3,7 +3,8 @@ import { IHttpService } from '../../presentation/protocols/http';
 
 export const adaptRoute = (endpoint: IHttpService) => {
   return async (req: Request, res: Response) => {
-    const { status, body } = await endpoint.handle();
+    const { query } = req;
+    const { status, body } = await endpoint.handle({ query });
     return res.status(status).send(body);
   };
 };
