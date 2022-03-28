5f31dda4e84768396dc3f4e6305063c3829286bf
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 15:12:39 -0300

    feature: map to domain model the google books api result

--- backend/src/infrastructure/repositories/books/google-books.ts
+++ backend/src/infrastructure/repositories/books/google-books.ts
@@ -1,6 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 import { IBooksRepository } from '../../../domain/contracts/books-repository';
 import { Book } from '../../../domain/models/book';
+
+export const toBook = (googleBook: any) => {
+  const postingYear = googleBook.volumeInfo?.publishedDate
+    ? new Date(googleBook.volumeInfo?.publishedDate).getFullYear()
+    : undefined;
+
+  return {
+    picture: googleBook.volumeInfo?.imageLinks?.thumbnail ?? undefined,
+    authors: googleBook.volumeInfo?.authors ?? ['Unknown author'],
+    categories: googleBook.volumeInfo?.categories ?? [],
+    title: googleBook.volumeInfo?.title ?? 'Unknown title',
+    rating: googleBook.volumeInfo?.averageRating ?? 0,
+    pagesCount: googleBook.volumeInfo?.pageCount ?? 0,
+    postingYear: postingYear ?? 0,
+    publisher: googleBook.volumeInfo?.publisher ?? 'Unknown publisher',
+    description: googleBook.volumeInfo?.description ?? '',
+  };
+};
 
 export class GoogleBooksRepository implements IBooksRepository {
   private readonly apiKey: string;
@@ -15,6 +34,8 @@ export class GoogleBooksRepository implements IBooksRepository {
   async get(): Promise<Book[]> {
     const url = `https://www.googleapis.com/books/v1/volumes?key=${this.apiKey}&maxResults=${this.pageLimit}`;
     const { data } = await axios.get(url);
-    return data ?? [];
+    return data?.items && Array.isArray(data?.items) && data?.items.length > 0
+      ? data.items.map((book: any) => toBook(book))
+      : [];
   }
 }
