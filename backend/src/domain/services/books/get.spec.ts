c254e9fd35cb47f83d7de6eaf38a9b605f6ec0ba
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 20:07:36 -0300

    test: should paginate and maintain previous behaviors on get books service

--- backend/src/domain/services/books/get.spec.ts
+++ backend/src/domain/services/books/get.spec.ts
@@ -2,6 +2,7 @@ import { GetBooksService } from './get';
 import { IBooksRepository } from '../../contracts/books-repository';
 import { MOCK_BOOKS } from './__mocks__/books';
 import { Book } from '../../models/book';
+import { Paginated } from '../../use-cases/pagination';
 
 interface ISutTypes {
   sut: GetBooksService;
@@ -11,9 +12,14 @@ interface ISutTypes {
 const makeBooksRepositoryStub = (): IBooksRepository => {
   class BooksRepositoryStub implements IBooksRepository {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    async get(search: string): Promise<Book[]> {
+    async get(search: string, page = 1): Promise<Paginated<Book>> {
       return new Promise((resolve) => {
-        resolve(MOCK_BOOKS);
+        resolve({
+          data: MOCK_BOOKS,
+          total: 2,
+          page: 1,
+          size: 5,
+        });
       });
     }
   }
@@ -31,21 +37,45 @@ describe('Get Books Services Test Suite', () => {
   it('Should call Books Repository to get books with provided search query', async () => {
     const { sut, booksRepositoryStub } = makeSut();
     const spy = jest.spyOn(booksRepositoryStub, 'get');
-    await sut.handle('mock-search-query');
-    expect(spy).toHaveBeenCalledWith('mock-search-query');
+    await sut.handle({
+      search: 'mock-search-query',
+      page: 2,
+    });
+    expect(spy).toHaveBeenCalledWith('mock-search-query', 2);
   });
 
-  it('Should provide static search query when anythings provided', async () => {
+  it('Should call Books Repository to get books from provided page', async () => {
     const { sut, booksRepositoryStub } = makeSut();
     const spy = jest.spyOn(booksRepositoryStub, 'get');
-    await sut.handle();
-    expect(spy).toHaveBeenCalledWith('flowers');
+    await sut.handle({
+      search: 'any',
+      page: 2,
+    });
+    expect(spy).toHaveBeenCalledWith('any', 2);
+  });
+
+  it('Should provide "flowers" search query when nothing is provided', async () => {
+    const { sut, booksRepositoryStub } = makeSut();
+    const spy = jest.spyOn(booksRepositoryStub, 'get');
+    await sut.handle({
+      page: 2,
+    });
+    expect(spy).toHaveBeenCalledWith('flowers', 2);
+  });
+
+  it('Should provide page "1" when nothing is provided', async () => {
+    const { sut, booksRepositoryStub } = makeSut();
+    const spy = jest.spyOn(booksRepositoryStub, 'get');
+    await sut.handle({
+      search: 'anything',
+    });
+    expect(spy).toHaveBeenCalledWith('anything', 1);
   });
 
   it('Should return books from the Books Repository', async () => {
     const { sut, booksRepositoryStub } = makeSut();
-    const expected = await booksRepositoryStub.get('any');
-    const response = await sut.handle();
+    const expected = await booksRepositoryStub.get('any', 1);
+    const response = await sut.handle({});
     expect(response).toEqual(expected);
   });
 });
