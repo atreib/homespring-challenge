3390a259f2e7fea6abdbc42ce8e6f9931c00ba63
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 17:08:41 -0300

    docs: setup swagger for endpoints documentation

--- backend/src/main/app.ts
+++ backend/src/main/app.ts
@@ -6,6 +6,18 @@ import { makeGetBooksEndpoint } from './factories/books/get';
 
 const app = express();
 app.use(cors());
+
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     produces:
+ *       - application/json
+ *     description: Get list of books from Google Books API
+ *     responses:
+ *       200:
+ *         description: Returns 5 books from Google Books API
+ */
 app.get('/books', adaptRoute(makeGetBooksEndpoint()));
 
 export { app };
--- backend/src/main/index.ts
+++ backend/src/main/index.ts
@@ -1,4 +1,20 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import { app } from './app';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Homespring Challenge',
+      version: '1.1.5',
+    },
+  },
+  apis: ['./src/main/*.ts'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const port = 3000;
 app.listen(port, () => {
