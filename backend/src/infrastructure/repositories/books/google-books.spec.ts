24e0be837dcb8074657500c11e100dd50e4cdfa6
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 11:27:32 -0300

    tests: create to-do test suite of books repository using google books api

---
+++ backend/src/infrastructure/repositories/books/google-books.spec.ts
@@ -0,0 +1,4 @@
+describe('Google Books API as Books Repository Test Suite', () => {
+  it.todo('Should connect with Google Books API');
+  it.todo('Should return the 5 first results from Google Books API');
+});
