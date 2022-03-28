5613bfef9dddeabd1bb851999d276ebdeea9e035
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 15:34:43 -0300

    test: should fetch from Get Books Service

--- backend/src/presentation/endpoints/books/get.spec.ts
+++ backend/src/presentation/endpoints/books/get.spec.ts
@@ -1,4 +1,34 @@
+import { Book } from '../../../domain/models/book';
+import { IGetBooksService } from '../../../domain/use-cases/books/get';
+import { GetBooksEndpoint } from './get';
+
+interface ISutTypes {
+  getBooksServiceStub: IGetBooksService;
+  sut: GetBooksEndpoint;
+}
+
+const makeGetBooksServiceStub = (): IGetBooksService => {
+  class GetBooksServiceStub implements IGetBooksService {
+    async handle(): Promise<Book[]> {
+      return new Promise((resolve) => resolve([] as Book[]));
+    }
+  }
+  return new GetBooksServiceStub();
+};
+
+const makeSut = (): ISutTypes => {
+  const getBooksServiceStub = makeGetBooksServiceStub();
+  const sut = new GetBooksEndpoint();
+  return { sut, getBooksServiceStub };
+};
+
 describe('Get Books Endpoint', () => {
-  it.todo('Should fetch from Get Books Service');
+  it('Should fetch from Get Books Service', async () => {
+    const { sut, getBooksServiceStub } = makeSut();
+    const spy = jest.spyOn(getBooksServiceStub, 'handle');
+    await sut.handle();
+    expect(spy).toHaveBeenCalled();
+  });
+
   it.todo('Should return the result from Get Books Service');
 });
