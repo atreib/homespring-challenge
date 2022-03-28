6c93c465b096b19685a4665f893c771ef70790da
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 16:31:19 -0300

    test: write to-do integration tests for the api

---
+++ backend/src/tests/books/get.spec.ts
@@ -0,0 +1,4 @@
+describe('Get Books Endpoint Integration Tests', () => {
+  it.todo('Should return 200 with the result of Google Books API if ok');
+  it.todo('Should return 500 with Internal Server Error if an error happens');
+});
