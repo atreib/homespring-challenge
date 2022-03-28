fa6c07151e8be8b38b3db439b940427ac3a544c6
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-28 13:22:26 -0300

    test: should use axios for fetching google books api

--- backend/src/infrastructure/repositories/books/google-books.spec.ts
+++ backend/src/infrastructure/repositories/books/google-books.spec.ts
@@ -1,7 +1,37 @@
+import axios from 'axios';
+import { GoogleBooksRepository } from './google-books';
+
+jest.mock('axios', () => ({
+  get: jest.fn(),
+}));
+
+interface SutTypes {
+  apiKey: string;
+  pageLimit: number;
+  sut: GoogleBooksRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const apiKey = 'mock-my-api-key';
+  const pageLimit = 5;
+  const sut = new GoogleBooksRepository();
+  return { sut, apiKey, pageLimit };
+};
+
 describe('Google Books API as Books Repository Test Suite', () => {
-  it.todo('Should fetch Google Books API using Axios');
-  it.todo('Should provide the injected Api Key');
-  it.todo('Should provide the injected limit per page');
+  beforeAll(() => {
+    axios.get = jest.fn().mockResolvedValue({ mock: true });
+  });
+
+  it('Should fetch Google Books API using Axios GET', async () => {
+    const spy = jest.spyOn(axios, 'get');
+    const { sut } = makeSut();
+    await sut.get();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it.todo('Should fetch using the injected Api Key');
+  it.todo('Should fetch using the injected limit per page');
   it.todo('Should fetch from the v1 of Google Books API');
   it.todo('Should return the result from Google Books API');
 });
