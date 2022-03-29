1f66327217d13c4db8a4256c3fa7f78d27877738
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-29 17:22:33 -0300

    feature: show fetched books on index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,34 @@
-import { NextPage } from "next";
+import { useState } from "react";
+import { serialize } from "superjson";
+import { BookCard } from "../components/domain/books/Card";
+import { ThemeLayout } from "../components/layout/Theme";
+import { Book } from "../models/book";
+import { getBooks } from "../services/books";
 
-const Home: NextPage = () => <h1 className="text-red-500">Hello world</h1>;
+interface HomePage {
+  books: Book[];
+}
+
+const Home = ({ books: initialBooksValue }: HomePage) => {
+  const [books] = useState<Book[]>(initialBooksValue);
+
+  return (
+    <ThemeLayout>
+      {books?.map((book, i) => (
+        <BookCard key={i} book={book} />
+      ))}
+    </ThemeLayout>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const books = (await getBooks()) ?? [];
+
+  return {
+    props: {
+      books: serialize(books).json,
+    },
+  };
+};
 
 export default Home;
