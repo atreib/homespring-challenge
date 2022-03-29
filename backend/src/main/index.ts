294bf573cbfd675a9fff0dfce64c2cce1f4df9d6
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-29 19:28:07 -0300

    chore: increase backend version

--- backend/src/main/index.ts
+++ backend/src/main/index.ts
@@ -7,7 +7,7 @@ const options = {
     openapi: '3.0.0',
     info: {
       title: 'Homespring Challenge',
-      version: '1.2.9',
+      version: '1.3.9',
       description: 'This is a showcase of a fullstack development',
     },
     servers: [
