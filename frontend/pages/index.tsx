5d5ff6d20f12b881f3a283a179fa7429def05749
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-29 10:30:51 -0300

    chore: setup tailwind

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
 
-const Home: NextPage = () => <h1>Hello world</h1>;
+const Home: NextPage = () => <h1 className="text-red-500">Hello world</h1>;
 
 export default Home;
