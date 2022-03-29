2c4a338c0945a7944af11b3ec091619406e3c11e
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-29 17:21:38 -0300

    feature: create book card component

---
+++ frontend/components/domain/books/Card.tsx
@@ -0,0 +1,28 @@
+import { Book } from "../../../models/book";
+
+interface BookCardProps {
+  book: Book;
+}
+
+const BookCard = ({ book }: BookCardProps) => (
+  <article className="flex flex-col space-y-4 border-b-2 mb-4 py-4">
+    <div className="flex space-x-4 md:space-x-8">
+      <div>
+        <img className="w-14" alt={book.title} src={book.picture} />
+      </div>
+      <div className="flex items-center justify-start">
+        <ul className="space-y-1">
+          <li>{book.authors.join(", ")}</li>
+          <li className="text-sm text-gray-400">
+            {book.categories.join(", ")}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <h1 className="text-xl font-bold text-gray-800">{book.title}</h1>
+    </div>
+  </article>
+);
+
+export { BookCard };
