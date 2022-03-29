e9e6fc85228facd1703a58ce5c1e422a1ccc86d8
Andre Treib <andre.schumacher.treib@gmail.com>
2022-03-29 15:20:18 -0300

    test: create mock for books list

---
+++ frontend/cypress/integration/__mocks__/books.ts
@@ -0,0 +1,30 @@
+import { Book } from "../../../models/book";
+
+export const MOCK_BOOKS: Book[] = [
+  {
+    picture:
+      "http://books.google.com/books/content?id=_oG_iTxP1pIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    authors: ["Daniel Keyes"],
+    categories: ["Fiction"],
+    title: "Flowers For Algernon",
+    rating: 4,
+    pagesCount: 336,
+    postingYear: 2007,
+    publisher: "HarperCollins",
+    description:
+      "Winner of both the Hugo and Nebula Awards, the powerful, classic story about a man who receives an operation that turns him into a genius...and introduces him to heartache. Charlie Gordon is about to embark upon an unprecedented journey. Born with an unusually low IQ, he has been chosen as the perfect subject for an experimental surgery that researchers hope will increase his intelligence-a procedure that has already been highly successful when tested on a lab mouse named Algernon. As the treatment takes effect, Charlie's intelligence expands until it surpasses that of the doctors who engineered his metamorphosis. The experiment appears to be a scientific breakthrough of paramount importance, until Algernon suddenly deteriorates. Will the same happen to Charlie?",
+  },
+  {
+    picture:
+      "http://books.google.com/books/content?id=8Pr_kLFxciYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    authors: ["Daniel Keyes"],
+    categories: ["Fiction"],
+    title: "Flowers For Algernon",
+    rating: 5,
+    pagesCount: 224,
+    postingYear: 2012,
+    publisher: "Hachette UK",
+    description:
+      "The classic novel about a daring experiment in human intelligence Charlie Gordon, IQ 68, is a floor sweeper and the gentle butt of everyone's jokes - until an experiment in the enhancement of human intelligence turns him into a genius. But then Algernon, the mouse whose triumphal experimental transformation preceded his, fades and dies, and Charlie has to face the possibility that his salvation was only temporary.",
+  },
+];
